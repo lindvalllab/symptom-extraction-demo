058359c4af6a76f655389d8fd8c5958bc0702892
DavisJ-DFCI <joshua_davis@dfci.harvard.edu>
2025-08-19 20:32:51 -0400

    Refactor demo.ipynb and llm.py to improve API key handling and response parsing; integrated dotenv for environment variables, updated model references, and enhanced error handling in chat completion function.

--- src/llm.py
+++ src/llm.py
@@ -3,27 +3,28 @@ import time
 from openai import OpenAI
 import openai
 
-from src.schema import OutputSchema
+#from src.schema import OutputSchema
 
 
 def openai_chat_completion_response(
         client: openai.OpenAI,
         system_message: str,
         user_message: str,
-        output_schema: OutputSchema,
-        model: str = 'gpt-3.5-turbo-1106',
+        output_schema:type,
+        model: str = 'gpt-4.1-nano-2025-04-14',
         max_attempts: int = 5,
         **kwargs,
 ):
     def _completion(messages: list[dict[str, str]]):
-        completion = client.chat.completions.create(
+        
+        completion = client.responses.parse(
             model=model,
-            messages=messages,
-            tools=[output_schema.tool],
+            input=messages,
+            text_format=output_schema,
             #tool_choice=output_schema.tool_choice,
             **kwargs,
         )
-        output = completion.choices[0].message.tool_calls[0].function.arguments
+        output = completion.output_text
         output = json.loads(output)
 
         return output
@@ -43,7 +44,9 @@ def openai_chat_completion_response(
             output_schema(**result)
             return result
         except Exception as e:
+            print(f"Error: {e}")
             print(f"Attempt {attempts} failed")
+            
             if attempts < max_attempts:
                 sleep_time = 2 ** (attempts - 2)  # Exponential backoff formula
                 print(f"Waiting {sleep_time} seconds before retrying...")
